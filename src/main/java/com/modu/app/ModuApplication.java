d93cd3cf282573af481e1e4abca4824d5c031753
Potato <gceyuna@gmail.com>
2023-07-06 15:50:40 +0900

    Initial commit

---
+++ src/main/java/com/modu/app/ModuApplication.java
@@ -0,0 +1,13 @@
+package com.modu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuApplication.class, args);
+	}
+
+}
