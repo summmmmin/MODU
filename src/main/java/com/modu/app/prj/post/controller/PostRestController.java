6cc377427c3440b03420f12816be846aa656d052
user <user@DESKTOP-ESV96G5>
2023-07-24 14:18:48 +0900

    0724 post

--- src/main/java/com/modu/app/prj/post/controller/PostRestController.java
+++ src/main/java/com/modu/app/prj/post/controller/PostRestController.java
@@ -5,14 +5,17 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.modu.app.prj.file.service.FileService;
+import com.modu.app.prj.post.service.MembDTO;
+import com.modu.app.prj.post.service.PostDTO;
 import com.modu.app.prj.post.service.PostService;
 import com.modu.app.prj.post.service.PostVO;
 
@@ -21,6 +24,9 @@ public class PostRestController {
 
 	@Autowired
 	PostService postService;
+	
+	@Autowired
+	FileService fileService;
 
 	// 전체조회
 	@GetMapping("posts/{bNum}")
@@ -33,7 +39,50 @@ public class PostRestController {
 	public PostVO postOne(@PathVariable("pNum") String postUniNo) {
 		return postService.getOnePost(postUniNo);
 	}
-
+	
+	// 등록
+//	@PostMapping("postInsert")
+//	public PostDTO postInsert(@RequestBody PostDTO postDTO, HttpSession session) {
+//		
+//		String particiMembUniNo = (String) session.getAttribute("particiMembUniNo");
+//		postDTO.setParticiMembUniNo(particiMembUniNo);
+//		
+//		PostVO postVO = new PostVO();
+//		postVO.setParticiMembUniNo(particiMembUniNo);
+//		postVO.setBrdUniNo(postDTO.getBrdUniNo());
+//		postVO.setTtl(postDTO.getTtl());
+//		postVO.setCm(postDTO.getCm());
+//		postVO.setPostTagArm(postDTO.getPostTagArm());
+//		
+//		//게시글등록
+//		postService.insertPost(postVO);
+//		
+//		//첨부파일등록
+//		MultipartFile[] file = postDTO.getAttFiles();
+//		System.out.println(file);
+//		fileService.insertFileWihtpost(file, postVO);
+//			
+//		return postDTO;
+//	}
+	
+	//멤버호출용리스트
+//	@GetMapping("brdMembs/{bNum}")
+//	@ResponseBody
+//	public List<MembDTO> chatCallMemb(@PathVariable("bNum") String brdUniNo, HttpSession session){
+//		
+//		//게시판정보조회
+//		PostVO postvo = postService.selectOneBoard(brdUniNo);
+//				
+//		char isPub = postvo.getPubcYn();
+//		if (isPub == 'Y') {
+//			String prjUniNo = (String) session.getAttribute("prjUniNo");
+//			return postService.selectCallMembPub(prjUniNo);
+//		} else if (isPub == 'N') {
+//			return postService.selectCallMembNonPub(brdUniNo);
+//		}
+//		return null;
+//	}
+	
 	//삭제
 	@GetMapping("postDelete/{pNum}")
 	public String postDelete(@PathVariable("pNum") String postUniNo) {
