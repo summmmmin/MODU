6401fec4cee043de73acf4cbdc37df2487d1f944
Potato <gceyuna@gmail.com>
2023-07-25 01:56:35 +0900

    회원가입 promise적용 (맞게쓴건가?)

--- src/main/resources/static/assets/js/custom_yuna.js
+++ src/main/resources/static/assets/js/custom_yuna.js
@@ -26,92 +26,64 @@ $('#sendVerificationCode').on('click', function () {
 // 인증번호 확인
 
 function verifySmsCode() {
-    var code = $('input[name="verificationCode"]').val();
-    console.log(code);
+    return new Promise((resolve, reject) => {
+        var code = $('input[name="verificationCode"]').val();
+        console.log(code);
 
-    var requestData = {
-        'code': code
-    };
+        var requestData = {
+            'code': code
+        };
 
-    $.ajax({
-        url: 'verifyCode',
-        type: 'POST',
-        data: JSON.stringify(requestData),
-        contentType: 'application/json',
-        success: function (response) {
-            console.log(response);
-            if (response.valid) {
-                alert("인증번호 확인에 성공했습니다.");
-                $('#signupButton').prop('disabled', false);
-            } else {
-                alert("인증번호가 틀렸습니다.");
-                $('#signupButton').prop('disabled', true);
+        $.ajax({
+            url: 'verifyCode',
+            type: 'POST',
+            data: JSON.stringify(requestData),
+            contentType: 'application/json',
+            success: function (response) {
+                console.log(response);
+                if (response.valid) {
+                    alert("인증번호 확인에 성공했습니다.");
+                    resolve(true);
+                } else {
+                    alert("인증번호가 틀렸습니다.");
+                    resolve(false);
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log(xhr.responseText);
+                alert("인증 에러");
+                reject(false);
             }
-        },
-        error: function (xhr, textStatus, errorThrown) {
-            console.log(xhr.responseText);
-            alert("Error occurred during the verification process. Please try again later.");
-            $('#signupButton').prop('disabled', true);
-        }
+        });
     });
 }
-// function verifySmsCode() {
-//     var code = $('input[name="verificationCode"]').val();
-//     console.log(code);
 
-//     var requestData = {
-//         'code': code
-//     };
+$('#signupButton').on('click', function (event) {
+    event.preventDefault(); // 기본 제출 동작을 방지
 
-//     $.ajax({
-//         url: 'verifyCode',
-//         type: 'POST',
-//         data: JSON.stringify(requestData),
-//         contentType: 'application/json',
-//         success: function (response) {
-//             console.log(response);
-//             if (response.valid) {
-//                 alert("인증번호 확인에 성공했습니다.");
-//                 $('#signupButton').prop('disabled', false);
-//             } else {
-//                 alert("인증번호가 틀렸습니다.");
-//             }
-//         },
-//         error: function (xhr, textStatus, errorThrown) {
-//             console.log(xhr.responseText);
-//             alert("Error occurred during the verification process. Please try again later.");
-//         }
-//     });
-// }
+    var email = $('input[name="id"]').val();
+    var name = $('input[name="nm"]').val();
+    var phoneNumber = $('input[name="phNo"]').val();
+    var password = $('input[name="pwd"]').val();
+    var confirmPassword = $('input[name="confirmPwd"]').val();
 
-$(document).ready(function () {
-    $('#signupButton').prop('disabled', true);
+    var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/;
 
-    $('input[name="pwd"], input[name="pwdConfirm"]').on('input', function () {
-        var password = $('input[name="pwd"]').val();
-        var confirmPassword = $('input[name="pwdConfirm"]').val();
-
-        var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
-
-        if (password === confirmPassword && password.match(passwordRegex)) {
-            $('#pwdError').text('');
-            $('#pwdConfirmError').text('');
-            $('#signupButton').prop('disabled', false); // Enable signup button when passwords match
-        } else {
-            $('#pwdError').text('비밀번호는 알파벳과 특수문자를 포함해서 최소 8자, 최대 16자로 작성해야 합니다.');
-            $('#pwdConfirmError').text('비밀번호가 일치하지 않습니다.');
-            $('#signupButton').prop('disabled', true); // Disable signup button when passwords don't match
-        }
-    });
-
-    $('form').on('submit', function (event) {
-        var password = $('input[name="pwd"]').val();
-        var confirmPassword = $('input[name="pwdConfirm"]').val();
-        var verificationCode = $('input[name="verificationCode"]').val();
-
-        if (password === '' || confirmPassword === '' || verificationCode === '') {
-            event.preventDefault();
-            alert('빈칸을 모두 채워주세요.');
-        }
-    });
+    if (!email || !name || !phoneNumber || !password || !confirmPassword) {
+        alert("빈칸을 모두 채워주세요.");
+        return false;
+    } else if (password !== confirmPassword) {
+        alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+        return false;
+    } else if (!passwordRegex.test(password)) {
+        alert("비밀번호는 영문자, 숫자, 특수 문자를 모두 포함한 8~16자 사이여야 합니다.");
+        return false;
+    } else {
+        verifySmsCode().then(isSmsVerified => {
+            if(isSmsVerified) {
+                $("form").submit(); // 모든 조건이 충족되면 폼 제출가능
+                alert("회원가입이 완료되었습니다.\n" + `환영합니다 ${name}님!`);
+            }
+        })
+    }
 });
