e906b2d107cfcec65073d43eb8cc8256a9d4f686
Sung hyeon <qpal0073@naver.com>
2023-07-25 19:39:51 +0900

    5555

--- src/main/java/com/modu/app/prj/bm/serviceimpl/BmServiceImpl.java
+++ src/main/java/com/modu/app/prj/bm/serviceimpl/BmServiceImpl.java
@@ -1,6 +1,5 @@
 package com.modu.app.prj.bm.serviceimpl;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +14,8 @@ public class BmServiceImpl implements BmService {
 
 	@Autowired
 	BmMapper bmMapper;
-	
-	
-	//게시판 즐겨찾기 등록
+
+	// 게시판 즐겨찾기 등록
 	@Override
 	public int BrdBmInsert(BmVO vo) {
 		int result = 0;
@@ -26,48 +24,45 @@ public class BmServiceImpl implements BmService {
 		// 값이 없으면 등록
 		if (BrdbmCount == 0) {
 			result = bmMapper.BrdBmInsert(vo);
-		// 값이 있으면 삭제
-		}else {
+			// 값이 있으면 삭제
+		} else {
 			result = bmMapper.BrdBmDelete(vo);
 		}
 		return result;
 	}
-	
-	//파일, 채팅, 댓글 즐겨찾기 등록
+
 	@Override
 	public int BmInsert(BmVO vo) {
-		int result = 0;
-		//값 조회
-		int BmCount = bmMapper.BmCount(vo);
-		System.out.println(BmCount);
-		System.out.println(vo);
-		// 값이 없으면 등록
-		if (BmCount == 0) {
-			result = bmMapper.BmInsert(vo);
-		// 없으면 삭제
-		}else {
-			result = bmMapper.BmDelete(vo);
-		}
-		return result;
+	    int result = 0;
+	    // 값 조회
+	    int BmCount = bmMapper.BmCount(vo);
+	    
+	    if (BmCount == 0) {
+	    	// 값이 없으면 등록
+	        result = bmMapper.BmInsert(vo);
+	    } else {
+	    	// 값이 있으면 삭제
+	        result = bmMapper.BmDelete(vo);
+	    }
+	    return result;
 	}
-	
+
 	@Override
 	public int BrdBmCount(BmVO vo) {
 		return bmMapper.BrdBmCount(vo);
 	}
-	
+
 	// 게시판 즐겨찾기 삭제
 	@Override
 	public int BmDelete(BmVO vo) {
 		return bmMapper.BrdBmDelete(vo);
 	}
 
-	
 	@Override
 	public List<BmVO> BmList(BmVO vo) {
 		return bmMapper.BmList(vo);
 	}
-	
+
 	@Override
 	public int BmCount(BmVO vo) {
 		return bmMapper.BmCount(vo);
--- src/main/java/com/modu/app/prj/board/controller/BoardController.java
+++ src/main/java/com/modu/app/prj/board/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.modu.app.prj.board.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -8,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.modu.app.prj.board.service.BoardService;
