2a281454adfd78f7515409148cdd763a02a0f832
jayeo <jayeo@JAYEONG-PC>
2023-07-26 08:41:33 +0900

    0725

--- src/main/java/com/modu/app/prj/file/controller/FileController.java
+++ src/main/java/com/modu/app/prj/file/controller/FileController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 
@@ -20,6 +21,8 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.modu.app.common.FileUtill;
@@ -67,26 +70,12 @@ public class FileController {
     }
     
     //img파일 미리보기??
-    @GetMapping("/images/{attNo}")
+    @GetMapping("/images/{attNo}") //이걸로대체됨?
     @ResponseBody
     public Resource showImage(String attNm, @PathVariable String attNo) throws MalformedURLException {
     	attNm = fileService.findFileById(attNo).getServerAttNm();
         return new UrlResource("file:" + (uploadPath+"/upload/") + attNm);
     }
-//    public ResponseEntity<byte[]> getFile(String attNo, String filePath){
-//    	FileVO fileVO = fileService.findFileById(attNo); //파일단건조회
-//    	File file = new File((uploadPath+"/upload")+File.separatorChar +filePath, attNo);
-//
-//        ResponseEntity<byte[]> result=null;
-//        try {
-//            HttpHeaders headers=new HttpHeaders();
-//            headers.add("Content-Type", Files.probeContentType(file.toPath()));
-//            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file),headers,HttpStatus.OK );
-//        }catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return result;
-//    }
     
     //첨부파일단건삭제(DB에서이름만삭제하는거임)
     @GetMapping("/delFile/{attNo}")
@@ -95,5 +84,51 @@ public class FileController {
     	fileService.deleteFile(attNo);
     	return 1;
     }
+    
+    //서버에서? 파일삭제?
+//    @PostMapping("/removeFile")
+//    public ResponseEntity<Boolean> removeFile(String attNo){
+//    	FileVO fileVO = fileService.findFileById(attNo); //파일단건조회
+//    	String fileName = fileVO.getServerAttNm();
+//        //String srcFileName = null;
+//
+//        //srcFileName = URLDecoder.decode(fileName,"UTF-8");
+//		//UUID가 포함된 파일이름을 디코딩해줍니다.
+//		File file = new File(uploadPath + File.separator + "upload", fileName);
+//		boolean result = file.delete();
 
+//            File thumbnail = new File(file.getParent()+file.getName());
+//            //getParent() - 현재 File 객체가 나태내는 파일의 디렉토리의 부모 디렉토리의 이름 을 String으로 리턴해준다.
+//            result = thumbnail.delete();
+//		if(result) {
+//			fileService.deleteFile(attNo);
+//		}
+//		return new ResponseEntity<>(result,HttpStatus.OK);
+//    }
+    
+    @PostMapping("/removeFile")
+    public ResponseEntity<Boolean> removeFile(@RequestBody String attNo) {
+        FileVO fileVO = fileService.findFileById(attNo); // 파일 단건 조회
+        if (fileVO == null) {
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
+        String fileName = fileVO.getServerAttNm();
+        try {
+            File file = new File(uploadPath + File.separator + "upload", fileName);
+            if (file.exists()) {
+                boolean result = file.delete(); // 서버에서 파일 삭제
+                if (result) {
+                    // 파일 삭제가 성공한 경우에만 DB에서 파일 정보를 삭제합니다.
+                    fileService.deleteFile(attNo);
+                }
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } else {
+                // 파일이 서버에 존재하지 않는 경우
+                return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
