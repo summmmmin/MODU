c50426975581dc29e69aa092243e606d1cf1ed78
user <user@DESKTOP-4RC0SJT>
2023-08-02 12:11:37 +0900

    151515

--- src/main/java/com/modu/app/prj/board/controller/BoardController.java
+++ src/main/java/com/modu/app/prj/board/controller/BoardController.java
@@ -98,15 +98,7 @@ public class BoardController {
 		return vo;
 	}
 	
-	// 해당 프로젝트 참여자 리스트 조회 후 참여된 회원에게 알람 발송을 위한 컨트롤
-//	@GetMapping("prjParticiMembList")
-//	public List<BoardVO> prjList(BoardVO vo, HttpSession session) {
-//	    String prjUniNo = (String) session.getAttribute("prjUniNo");
-//	    vo.setPrjUniNo(prjUniNo);
-//	    boardService.prjList(vo);
-//	    System.out.println(boardService.prjList(vo));
-//	    return boardService.prjList(vo);
-//	}
+
 	
 
 }
